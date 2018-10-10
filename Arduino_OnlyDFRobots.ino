e9080eba3f263d1096b24065d7fe24aa233c0e70
Unknown <gioo27_10@tiscali.it>
2018-10-10 16:26:55 +0200

    Added arduino

---
+++ Arduino_OnlyDFRobots.ino
@@ -0,0 +1,14 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // stop the program for <sensorValue> milliseconds:
+  delay(1000);
+  Serial.println(sensorValue);
+}
